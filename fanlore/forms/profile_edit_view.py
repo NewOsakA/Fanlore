ce12621031563a358958c7a03ca19238f8c744cc
Kanisorn KANOKRATANA <kanisorn506@gmail.com>
2025-02-12 20:19:17 +0700

    Update proflie edit

--- fanlore/forms/profile_edit_view.py
+++ fanlore/forms/profile_edit_view.py
@@ -26,23 +26,25 @@ class ProfileUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ["email", "username"]  # Editable fields
+        fields = ["first_name", "last_name","email", "username"]
         widgets = {
             "email": forms.EmailInput(attrs={"class": "form-control"}),
             "username": forms.TextInput(attrs={"class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
         }
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
-        if User.objects.filter(email=email).exclude(
-                pk=self.instance.pk).exists():
+        if User.objects.filter(email=email)\
+                .exclude(pk=self.instance.pk).exists():
             raise ValidationError("This email is already in use.")
         return email
 
     def clean_username(self):
         username = self.cleaned_data.get("username")
-        if User.objects.filter(username=username).exclude(
-                pk=self.instance.pk).exists():
+        if User.objects.filter(username=username)\
+                .exclude(pk=self.instance.pk).exists():
             raise ValidationError("This username is already taken.")
         return username
 
@@ -58,7 +60,8 @@ class ProfileUpdateForm(forms.ModelForm):
                     "You must enter your current password to change it.")
             if not self.instance.check_password(old_password):
                 raise ValidationError("The current password is incorrect.")
-            if new_password1 and new_password2 and new_password1 != new_password2:
+            if new_password1 and new_password2 and\
+                    new_password1 != new_password2:
                 raise ValidationError("New passwords do not match.")
             password_validation.validate_password(new_password1, self.instance)
 
