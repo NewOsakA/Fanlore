992ed37332abd88967fcf10118bf9a324bbf3485
Kanisorn KANOKRATANA <kanisorn506@gmail.com>
2025-03-31 14:55:43 +0700

    Fix bug on event form

--- fanlore/forms/event_create_form.py
+++ fanlore/forms/event_create_form.py
@@ -2,6 +2,7 @@ import cloudinary.uploader
 from django import forms
 from pagedown.widgets import PagedownWidget
 from fanlore.models import Event
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 class EventCreateForm(forms.ModelForm):
@@ -25,11 +26,12 @@ class EventCreateForm(forms.ModelForm):
 
     def save(self, commit=True):
         event = super().save(commit=False)
-        banner_image = self.cleaned_data.get("banner_image")
 
-        if banner_image:
+        # Only upload a new image if a new one is uploaded
+        if self.cleaned_data.get("banner_image") and isinstance(
+                self.cleaned_data["banner_image"], InMemoryUploadedFile):
             uploaded = cloudinary.uploader.upload(
-                banner_image,
+                self.cleaned_data["banner_image"],
                 folder="event_banners/",
                 public_id=f"event_{event.pk or 'new'}",
                 overwrite=True,
