067a347956f28ac1b9d39aecf90a798172f3a841
Natthadit LERTPISANWUT <act23107@yahoo.com>
2025-04-02 21:59:03 +0700

    add pagination in Bookmark page

--- fanlore/views/bookmark_list_view.py
+++ fanlore/views/bookmark_list_view.py
@@ -2,11 +2,11 @@ from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from ..models import Bookmark, Content
 
-
 class BookmarkedPostsView(LoginRequiredMixin, ListView):
     model = Content
     template_name = 'fanlore/bookmarked.html'
     context_object_name = 'content_list'
+    paginate_by = 5
 
     def get_queryset(self):
         user = self.request.user
